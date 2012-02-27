ee5903d9f24b3c920b56ebfc8e79fbced094f3d0
josejuan <dev@computer-mind.com>
2012-02-27 15:52:48 +0100

    demo particles finished

--- demos/simple.particles.js
+++ demos/simple.particles.js
@@ -7,92 +7,74 @@ demos.AttachDemo(new H5GL.BoxBound({
 
 	demo: {
 		title: "Simple Particles",
-		description: "Show how to use <b>H5GL. BoxBoundForces</b> to extend a <b>H5GL. BoxBoundBox</b> object and simulate (very simple) physics. If you need a more complex physics schema, I recommend to you use <b>Box2D</b> engine.",
+		description: "Show how to use <b>H5GL. BoxBoundForces</b> to extend a <b>H5GL. BoxBoundBox</b> object and simulate (very simple) physics. <b>H5GL.BSpline</b> is used for particles path.",
 		sourcecode: "demos/simple.particles.js",
 		thumbnail: "demos/simple.particles.png"
 	},
 
 	started: false,
 	NUM_PARTICLES: 290,
-	PARTICLE_WIDTH: 10,
-	last_mouse_pos: {x: 0, y: 0},
-	floor_box: null,
+	breset: 0,
 
-	onMouseMove: function(s, d) {
-		this.last_mouse_pos = d;
+	onKeyUp: function(s, d) {
+		if(d.keyCode == 72)
+			this.stateA = !this.stateA;
+		this.stateB = d.keyCode == 74;
 	},
 
 	onControllerActivation: function(s, d) {
-		var controller = this;
-
 		if(!this.started) {
-
 			this.started = true;
-			s.__bcontext2d.strokeStyle = 'black';
 
-			var colors = ['yellow', 'green', 'orange', 'blue'];
+			// simple image loader
+			var colors = ['red', 'green', 'blue', 'orange', 'purple'];
+			for(var n = 0; n < colors.length; n++) {
+				var i = new Image();
+				i.src = 'demos/resources/particles/' + colors[n] + '.png';
+				colors[n] = i;
+			}
+
 			var w = s.__canvas.width, h = s.__canvas.height;
 
-			this.last_mouse_pos = {x: w >> 1, y: h >> 3};
-
-			this.AttachBox(
-				this.floor_box =
-					new H5GL.BoxBoundForces(10,
-						new H5GL.BoxBoundBox(
-							-10000, h + this.PARTICLE_WIDTH, 10000, 10000, 0,
-							{	bcolor: colors[n % colors.length],
-								color: colors[n % colors.length],
-								colliders: [],
-								v: {x: 0, y: 0}
-							}
-						)
-					)
-			);
-
 			this.onDefaultDraw = function (b, s, d) {
-				d.context.fillStyle = this.color;
-				var p = this.position;
-				d.context.fillRect( p.xi, p.yi, p.xf - p.xi, p.yf - p.yi );
-				d.context.strokeRect( p.xi, p.yi, p.xf - p.xi, p.yf - p.yi );
-			};
-			this.onDefaultCollideIn = function(b, s, bc) {
-				if(bc == b.floor_box) {
-					var dx = b.last_mouse_pos.x - (this.position.xf + this.position.xi) * 0.5;
-					var dy = b.last_mouse_pos.y - (this.position.yf + this.position.yi) * 0.5;
-					this.position.xi += dx;
-					this.position.xf += dx;
-					this.position.yi += dy;
-					this.position.yf += dy;
-					var a = s.rndf(Math.PI / 4, 3 * Math.PI / 4);
-					this.v = {x: 40 * Math.cos(a), y: -90 * Math.sin(a)};
+				if(this.alpha > 0) {
+					this.alpha -= 0.005;
+					d.context.globalAlpha = this.alpha;
+					d.context.drawImage(this.color, this.position.xi, this.position.yi, this.w, this.w);
+					d.context.globalAlpha = 0.0;
 				}
 			};
 			this.onDefaultAnimate = function(b, s, d) {
 				this.ApplyForces(d.F, d.R, d.t);
 			};
 
-			var x = w >> 1, y = h >> 3, n = 1;
-			var dy = (h - y) / (1.0 * this.NUM_PARTICLES);
-			while(n < this.NUM_PARTICLES) {
-
-				var bw = this.PARTICLE_WIDTH * s.rndf(0.75, 1);
+			var x = w >> 1, n = -1;
+			while(++n < this.NUM_PARTICLES)
 				this.AttachBox(
 					new H5GL.BoxBoundForces(10,
 						new H5GL.BoxBoundBox(
-							x, y, x + bw, y + this.PARTICLE_WIDTH * s.rndf(0.75, 1), n,
-							{	bcolor: colors[n % colors.length],
+							x, h, x + 1, h + 1, n,
+							{
 								color: colors[n % colors.length],
-								colliders: [],
-								v: {x: 0, y: 1 * n}
+								alpha: 0.0,
+								v: {x: 0, y: n},
+								index: n,
+								w: s.rnd(8, 16)
 							}
 						)
 					)
 				);
 
-				y += dy;
-				n++;
-			}
-
+			var m = Math.min(w, h) >> 4;
+			this.points = [
+				{x:     m, y:     m}, {x: m, y: m}, {x: m, y: m},
+				{x: w - m, y:     m}, {x: w - m, y:     m},
+				{x:     m, y: h - m},
+				{x: w - m, y: h - m},
+				{x:     m, y:     m}, {x: m, y: m}, {x: m, y: m}
+			];
+			this.bspline = new H5GL.BSpline(this.points, false);
+			this.bspline.ComputeLengths(0.99, 200);
 		}
 
 		this.lastTime = s.currentTime;
@@ -100,16 +82,28 @@ demos.AttachDemo(new H5GL.BoxBound({
 
 	onDraw: function(s, d) {
 
+		var ct = s.currentTime;
+
 		d.context.clearRect(0, 0, d.canvas.width, d.canvas.height); // clear entire viewport
 
-		this.AnimateBoxes(s, {
-			F: {x: 0, y: 9.8 + 5},
-			R: {x: 0.999, y: 0.999},
-			t: s.currentTime - this.lastTime
-		});
-		this.lastTime = s.currentTime;
-
+		d.context.globalAlpha = 0.0;
 		this.DrawBoxes(s, d);
+
+		{ // reset particle
+			this.breset = (this.breset + 1) % this.__box_list.length;
+			var t = ct; t *= 0.2; t = t - ~~t; // spline time
+			var p = this.bspline.GetNormalizedPoint(t);
+			var b = this.__box_list[this.breset];
+			var a = s.rndf(Math.PI / 4, 3 * Math.PI / 4);
+			b.position.xi = p.x; b.position.xf = p.x + 1;
+			b.position.yi = p.y; b.position.yf = p.y + 1;
+			b.v = {x: 40 * Math.cos(a), y: -90 * Math.sin(a)};
+			b.alpha = 1.0;
+		}
+
+		this.AnimateBoxes(s, {F: {x: 0, y: 9.8 + 5}, R: {x: 0.999, y: 0.999}, t: ct - this.lastTime});
+		this.lastTime = ct;
+		d.context.globalAlpha = 1.0;
 
 	}
 
