4c174936d8bbd8964ecaa6252d723bd857e31b92
josejuan <dev@computer-mind.com>
2012-02-29 17:13:12 +0100

    in progress

--- games/sokobullban/sokobullban_maps.js
+++ games/sokobullban/sokobullban_maps.js
@@ -8,11 +8,43 @@
     Copyright 2012 (all right reserved)
     jose-juan(at)computer-mind.com
 
+==============================================
+
+	Map symbols:
+
+		"·", empty cell
+		"X", solid wall
+		"*", bulldozer start point
+		"-", objetive platform
+		"|", block to move to objetive platform
+		"+", block over a objetive platform
+
  */
 
 var sokomaps = [
 
-	{
+	{	id: "test1",
+		name: "Test map 1",
+		description: "A simple test map",
+		map: "\
+XXXXXXXXXX\n\
+X···XX··*X\n\
+X········X\n\
+X---XX|||X\n\
+XXXXXXXXXX\n\
+"
+	},
+
+	{	id: "test2",
+		name: "Test map 2",
+		description: "Other simple test map",
+		map: "\
+·XXX··XXX·\n\
+X-··XX··*X\n\
+X········X\n\
+X·--XX|||X\n\
+·XXX··XXX·\n\
+"
 	}
 
 ];
